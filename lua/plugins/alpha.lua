16ed862840b0eb15db2466c68eaa0f30920dab39
Julihermes Melo <julihermes@hotmail.com>
2024-01-02 20:50:44 -0300

    Add Alpha dashboard.

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,41 @@
+return {
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+    }
+
+    dashboard.section.buttons.val = {
+  		dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+			dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+			dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+			dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+			dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+			dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+		}
+
+
+    alpha.setup(dashboard.opts)
+  end,
+}
