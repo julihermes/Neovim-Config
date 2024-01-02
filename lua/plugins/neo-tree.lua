4cf3943460824ddc44d911834404fa7de0029e07
Julihermes Melo <julihermes@hotmail.com>
2024-01-02 20:24:40 -0300

    Add neo-tree.

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,24 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+    require("neo-tree").setup({
+        close_if_last_window = true,
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_by_name = {
+              "node_modules",
+              ".git"
+            },
+          }
+        }
+    })
+		vim.keymap.set("n", "\\", ":Neotree filesystem reveal left<CR>", {})
+	end,
+}
