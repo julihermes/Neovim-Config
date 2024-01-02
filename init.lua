8bdc977b86b7282f50f9ce730c9a7cf6b35b279d
Julihermes Melo <julihermes@hotmail.com>
2024-01-02 20:09:03 -0300

    Add Telescope. Add some configs

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.wo.number = true
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -5,7 +13,7 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
