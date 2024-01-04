54e317244f719acadc8a9bbeb691e15d0dbf5ad5
Julihermes Melo <julihermes@hotmail.com>
2024-01-04 16:05:36 -0300

    Add Highlight on yank

--- lua/options.lua
+++ lua/options.lua
@@ -2,8 +2,7 @@ vim.g.mapleader = ' '                   -- Set leader key
 vim.g.maplocalleader = ' '              -- Set local leader key
 vim.o.list = true                       -- Show Whitespace trail
 vim.o.mouse = 'a'                       -- Enable mouse mode
-vim.o.breakindent = true                -- Enable break indent
-vim.o.ignorecase = true                 -- Case-insensitive searching
+vim.o.breakindent = true                -- Enable break indent vim.o.ignorecase = true                 -- Case-insensitive searching
 vim.o.smartcase = true                  -- UNLESS \C or capital in search
 vim.o.updatetime = 250                  -- Decrease update time
 vim.o.timeoutlen = 300                  -- Decrease update time
@@ -17,3 +16,13 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
