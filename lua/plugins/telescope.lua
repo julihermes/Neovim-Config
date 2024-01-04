31db7eb9eedf07efcbc255b3e9cdc1d402a1e43d
Julihermes Melo <julihermes@hotmail.com>
2024-01-04 13:34:52 -0300

    Add fzf telescope extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,21 @@
 return {
   {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
     config = function()
-      require("telescope").setup({
+      local telescope = require("telescope")
+      telescope.setup({
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
@@ -18,7 +26,8 @@ return {
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
-      require("telescope").load_extension("ui-select")
+      telescope.load_extension("ui-select")
+      telescope.load_extension("fzf")
     end,
   },
 }
