e3ce843746ce14745a4c4deff095bbb5cfe386fd
Julihermes Melo <julihermes@hotmail.com>
2024-04-08 20:53:29 -0300

    Added catppuchin theme to more plugins

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -6,6 +6,15 @@ return {
     require("catppuccin").setup({
       flavour = "mocha",
       transparent_background = true,
+      integrations = {
+        fidget = true,
+        harpoon = true,
+        mason = true,
+        which_key = true,
+        indent_blankline = {
+          enabled = true,
+        },
+      },
     })
     vim.cmd.colorscheme("catppuccin")
   end,
