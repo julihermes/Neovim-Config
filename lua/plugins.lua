94d9bcf2ee4a8fe975d910f21dd1715c15bc00a7
Julihermes Melo <julihermes@hotmail.com>
2024-04-18 18:02:13 -0300

    Change theme to Tokyo Night.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,7 +1,7 @@
 return {
   { "tpope/vim-sleuth" },
   { "numToStr/Comment.nvim", opts = {}, lazy = false },
-  { "nvim-lualine/lualine.nvim", opts = { options = { theme = "catppuccin", ignore_focus = { "sfm" } } } },
+  { "nvim-lualine/lualine.nvim", opts = { options = { theme = "tokyonight", ignore_focus = { "sfm" } } } },
   { "fedepujol/move.nvim", opts = {} },
   { "gbprod/substitute.nvim", opts = {} },
   { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, opts = {} },
